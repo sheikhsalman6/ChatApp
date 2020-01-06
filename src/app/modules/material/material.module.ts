cd10a51b6cfeb991d7b0394a305c91ab52eb8be0
Salman Sheikh <sheikhsalman6@gmail.com>
2020-01-06 13:30:54 +0530

    added auth module

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -1,10 +1,26 @@
 import {NgModule} from '@angular/core';
 import {MatSliderModule} from '@angular/material/slider';
 import {MatTabsModule} from '@angular/material/tabs';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatIconModule} from '@angular/material/icon';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 const materialComponent = [
   MatSliderModule,
-  MatTabsModule
+  MatTabsModule,
+  MatCardModule,
+  MatToolbarModule,
+  MatGridListModule,
+  MatInputModule,
+  MatButtonModule,
+  MatDialogModule,
+  MatIconModule,
+  MatProgressSpinnerModule
 ];
 
 
